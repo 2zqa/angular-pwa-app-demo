9d3cb8cc697f6f1b09727498fddef7f57a899c79
2zqa <25235249+2zqa@users.noreply.github.com>
2021-06-04 02:46:11 +0200

    Add update prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Routes} from "@angular/router";
 import {AboutComponent} from "./about/about.component";
+import {CheckUpdateService} from "./check-update.service";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,16 @@ import {AboutComponent} from "./about/about.component";
 })
 export class AppComponent {
   title = 'angular-pwa-app';
+  updateAvailable: boolean = false;
 
+  ngOnInit() {
+    //subscribe in the component to the current message
+    this.checkForUpdateService.updateAvailable.subscribe(updateAvailable => this.updateAvailable = updateAvailable);
+  }
+
+  constructor(private checkForUpdateService: CheckUpdateService) {}
+
+  runUpdate() {
+    document.location.reload();
+  }
 }
